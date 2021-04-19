893256c7ade123422948f4fe26ccc85d9bc333a3
admin <admin@DESKTOP-8L2IG92>
2021-04-20 06:34:31 +0900

    ex3no13

---
+++ ex3no13/src/ex3no13/ex3no13.java
@@ -0,0 +1,14 @@
+package ex3no13;
+
+public class ex3no13 {  //103p
+	public static void main(String[] args) {
+		char c1 = 'a';
+		
+//		char c2 = c1+1;
+		char c2 = 'a'+1;
+		
+		System.out.println(c2);
+		
+	}
+
+}
